826eba0117b013770a814afc2cab200e37e8ff4c
MUKONGE ERIC <mukonge.eric@gmail.com>
2023-03-29 11:43:22 +0300

    Update

---
+++ car.js
@@ -0,0 +1,35 @@
+var carName = "Defender";
+var carWeight = 23;
+
+if (carWeight > 20) {
+    console.log(carName + " says DOO DOO");
+} else {
+    console.log(carName + " says doo doo");
+}
+
+carName = "Forester";
+carWeight = 13;
+
+if (carWeight > 20) {
+    console.log(carName + " says SOO SOO");
+} else {
+    console.log(carName + " says soo soo");
+}
+
+carName = "Truck";
+carWeight = 53;
+
+if (carWeight > 20) {
+    console.log(carName + " says VUU VUU ");
+} else {
+    console.log(carName + " says vuu vuu");
+}
+
+carName = "Bike";
+carWeight = 17;
+
+if (carWeight > 20) {
+    console.log(carName + " says BOO BOO");
+} else {
+    console.log(carName + " says boo boo");
+}
---
+++ paraments & arguments/car.js
@@ -0,0 +1,11 @@
+function car(name, weight){
+    if (weight > 20) {
+        console.log(name + " says VUU VUU");
+    } else {
+        console.log(name + " says vuu vuu");
+        }
+}
+car("Defender", 23);
+car("Forester", 13);
+car("Truck", 53);
+car("Bike", 17);
